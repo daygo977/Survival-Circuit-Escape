337ce2e9742ecc4c9f85d8aaba6be747f4118e5e
DiegoM <dmorales7072@sdsu.edu>
2025-10-21 16:38:45 -0700

    Created EnemyHealth.cs

---
+++ Assets/Scripts/Game/Enemy/EnemyHealth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+//public class Health : MonoBehaviour
+//{
+//    public int hp = 3;
+//
+//    public void TakeDamage(int amount)
+//    {
+//        hp -= amount;
+//        if (hp <= 0)
+//        {
+//            gameObject.SetActive(false);
+//        }
+//    }
+//
+//}
+//
