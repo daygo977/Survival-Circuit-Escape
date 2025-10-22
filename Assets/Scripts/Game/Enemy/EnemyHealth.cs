86094f0cff7b3dabd451de7df1c73fbce7d513b4
daygo977 <dmorales7072@sdsu.edu>
2025-10-21 20:06:58 -0700

    fix(add better naming for EnemyHealth.cs): Same as previous commit, naming in EnemyHealth.cs was ambiguous. Changed file name from Health.cs to EnemyHealth.cs, and also TakeDamage function to EnemyTakeDamage

--- Assets/Scripts/Game/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Game/Enemy/EnemyHealth.cs
@@ -2,19 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//
-//public class Health : MonoBehaviour
-//{
-//    public int hp = 3;
-//
-//    public void TakeDamage(int amount)
-//    {
-//        hp -= amount;
-//        if (hp <= 0)
-//        {
-//            gameObject.SetActive(false);
-//        }
-//    }
-//
-//}
-//
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int hp = 3;
+
+    public void EnemyTakeDamage(int amount)
+    {
+        hp -= amount;
+        if (hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
